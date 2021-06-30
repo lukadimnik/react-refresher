47710515d39c28a457e09ff50f46322ca4b9125b
Luka Dimnik <lukadimnik@pop-os.localdomain>
2021-06-30 18:34:27 +0300

    added form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );

d3e8fd0c5c8bd3fc026fbdd4f52e95270823c426
Luka Dimnik <lukadimnik@pop-os.localdomain>
2021-06-30 14:59:41 +0300

    created custom wrapper "Card"

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
@@ -26,7 +27,7 @@ const Expenses = (props) => {
         amount={expenses[3].amount}
         date={expenses[3].date}
       />
-    </div>
+    </Card>
   );
 };
 
